44bcf5162e5f10d334f6255aa26b818c7f267343
escotoj <j.escoto14@gmail.com>
2023-03-02 18:48:34 -0800

    update to the code to render question and identify button selected

---
+++ app.js
@@ -0,0 +1,147 @@
+// var qOne = [
+//   {
+//     question: "What was professor snapes first name?",
+//     choices: ["Severus", "Black", "Severe", "sirus"],
+//     correctAnswer: 0,
+//   },
+// ];
+
+// var qTwo = [
+//   {
+//     question: "What kind of pet did Ron have?",
+//     choices: ["Mouse", "Duck", "Rat", "Bird"],
+//     correctAnswer: 2,
+//   },
+// ];
+
+// var qThree = [
+//   {
+//     question: "What kind of pet did Ron have?",
+//     choices: ["Mouse", "Duck", "Rat", "Bird"],
+//     correctAnswer: 2,
+//   },
+// ];
+
+// var qFour = [
+//   {
+//     question: "Who was the most powerfull wizard?",
+//     choices: ["Albus", "Tom", "Grinderwal", "Harry"],
+//     correctAnswer: 0,
+//   },
+// ];
+
+// var qFive = [
+//   {
+//     question: "What kind of house did teh Weasleys live in?",
+//     choices: ["Mansion", "Duplex", "Bourough", "Flat"],
+//     correctAnswer: 2,
+//   },
+// ];
+
+// const start = document.getElementById("start");
+const answers = document.querySelectorAll("answers");
+const question = document.getElementById("questionPromp");
+var choiceA = document.getElementById("choiceA")
+var choiceB = document.getElementById("choiceB")
+var choiceC = document.getElementById("choiceC")
+var choiceD = document.getElementById("choiceD")
+
+// if (questionPrompt[questionIndex].correct) {
+// }
+// event.target.id
+
+var questionPromp = [
+  {
+    question: "What does HTML stand for?",
+    choiceA: "Correct",
+    choiceB: "Wrong",
+    choiceC: "Wrong",
+    correct: "choiceA",
+  },
+  {
+    question: "What does CSS stand for?",
+    choiceA: "Wrong",
+    choiceB: "Correct",
+    choiceC: "Wrong",
+    correct: "B",
+  },
+  {
+    question: "What does JS stand for?",
+    choiceA: "Wrong",
+    choiceB: "Wrong",
+    choiceC: "Correct",
+    correct: "C",
+  },
+];
+
+const lastQuestion = questionPromp.length - 1;
+
+// index
+var questionIndex = 0;
+
+console.log(question)
+
+function renderQuestion() {
+  var q = questionPromp[questionIndex];
+  question.textContent = q.question;
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+}
+
+var answersClick = document.querySelector("section");
+
+answersClick.addEventListener("click", function (event) {
+  var buttonClickID = event.target.id;
+  console.log(buttonClickID);
+  renderQuestion();
+});
+
+var firstChildUl = document.getElementById("first-child-ul");
+console.log(firstChildUl);
+
+// use nagivate function from example 18 to sort through all questions, do not use for-loop for this.
+// function navigate(direction) {
+//     index = index + direction;
+//     if (index < 0) {
+//       index = images.length - 1;
+//     } else if (index > images.length - 1) {
+//       index = 0;
+//     }
+//     currentImage = images[index];
+//     carousel.style.backgroundImage = "url('" + currentImage + "')";
+// }
+
+// need to add these question to the html by targeting elements and get by id fucntions
+// putting an event listern as a target, will be captured var imageContainer = document.querySelector(".img-container");
+// Listen for any clicks within the img-container div imageContainer.addEventListener("click", function(event) {var element = event.target;
+// var imageContainer = document.querySelector(".img-container");
+
+// Check if the clicked element was an image
+// if (element.matches("1")) {
+//   // Get the current value of the image's data-state attribute
+//   var state = element.getAttribute("data-state");
+
+//   if (state === "2") {
+//     // Change the data-state attribute's value
+//     // There are two different ways this attribute can be set
+//     element.dataset.state = "3";
+//     element.setAttribute("data-state", "animate");
+//     // Update the image's source to the string being stored in the data-animate attribute
+//   } else {
+//     // Change the attributes back to their non-animated values
+//     element.dataset.state = "4";
+//     element.setAttribute("src", element.dataset.still);
+//   }
+// }
+
+// var answers = document.querySelector("section");
+
+// answers.addEventListener("click", function (event) {
+//   var element = event.target;
+//   console.log("hello");
+// });
+
+// function log() {
+//     var element = console.log("hello");
+// }
